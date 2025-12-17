8b2ab7ac57f9f1535daf23c9f3d1fe6a08ac9d11
yuvraj singh <79921222+yuvraj-singh-codes@users.noreply.github.com>
2025-12-17 18:23:18 +0530

    feat: implement Configure Node.js server

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// MongoDB connection
+const mongoURI = 'your_mongodb_connection_string_here';
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
+// Sample route
+app.get('/', (req, res) => {
+    res.send('Welcome to the Node.js server!');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
